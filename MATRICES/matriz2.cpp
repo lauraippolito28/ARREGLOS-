5b03db59c20e091f19151ed72c96d4490dae0a3c
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-02-14 14:47:34 -0500

    ACTIVIDAD EN CLASE

---
+++ MATRICES/matriz2.cpp
@@ -0,0 +1,10 @@
+// Maquina dispensadora con nombre, precio  y posicion (punto 2)
+#include <iostream> 
+#include <vector> 
+using namespace std;
+
+int main(){
+    string maquina [3][3]; 
+
+    maquina[0][0]="1";
+}
---
+++ MATRICES/matriz3.cpp
@@ -0,0 +1,9 @@
+// Juego de triqui 3x3 (punto 3) 
+#include <iostream> 
+#include <vector> 
+using namespace std; 
+
+int main(){
+  string triqui [3][3]; 
+  
+}
