449f128729987b6e559284b153a3cf9ad3a560ef
lauraippolito28 <159057618+lauraippolito28@users.noreply.github.com>
2024-02-16 22:43:25 -0500

    Update triqui.cpp

---
+++ ACTIVIDAD DE CLASE 14-02-24/triqui.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int main(){
+    string triqui [3][3]; 
+    int filas; 
+    string eleccion; 
+
+     for (int i = 0; i < 3; i++);
+     for (int j = 0; j < 3; j++);
+
+     cout << "Escoge entre X  y O" << eleccion << endl;
+    
+    return 0;
+}
